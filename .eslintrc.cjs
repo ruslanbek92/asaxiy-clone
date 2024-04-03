b0e5952f28d051ac877cb0c603c5576cdef37cf1
ruslanbek normurodov <aksirus89@gmail.com>
2024-04-03 11:55:17 +0500

    feat:check husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,5 +20,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "react/react-in-jsx-scope": "off",
+  "react/jsx-uses-react": "off",
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-
-
-import './App.css'
+import './App.css';
 
 function App() {
-  return (
-    <>
-    </>
-  )
+    return <div>App</div>;
 }
 
 export default App;
